1cb4f1e5500f74241ca8d4ed35d2148556512321
Brad Beltowski <bbelowski@fullsail.com>
2017-02-14 16:47:42 -0500

    Fixing crappy auto merge

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 const gulp    = require('gulp');
 const concat  = require('gulp-concat');
-const connect = require('gulp-connect');
 const plumber = require('gulp-plumber');
 const notify  = require('gulp-notify');
 const nodemon = require('gulp-nodemon');
 const jshint  = require('gulp-jshint');
 const lrPort  = 35729;
-
 
 const paths = {
   assets: ['./src/assets/'],
@@ -15,25 +13,29 @@ const paths = {
     './src/**/*.js'
   ],
   html: [
-  	'./src/html/*.html',
-  	'./src/html/index.html'
+    './src/html/*.html',
+    './src/html/index.html'
   ],
   styles: [
-  	'./src/css/styles.css',
-  	'./src/css/*.css'
+    './src/css/styles.css',
+    './src/css/*.css'
   ]
 };
 
-gulp.task('serve', function () {
-    browserSync.init({
-        server: {
-            baseDir: "./public"
-        }
-    });
+gulp.task('serve', function(){
+  nodemon({'script': './server.js'});
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
 });
 
 gulp.task('js:lint', function(){
   return gulp.src(paths.scripts)
+    .pipe(plumber())
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(notify({message: 'jshint done'}));
@@ -41,45 +43,37 @@ gulp.task('js:lint', function(){
 
 gulp.task('js:build', function(){
   return gulp.src(paths.scripts)
+    .pipe(plumber())
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./public/js'))
+    .pipe(connect.reload())
     .pipe(notify({message: 'JS concated'}));
-});
-
-gulp.task('js:watch', ['js:build'], function (done) {
-    browserSync.reload();
-    done();
 });
 
 gulp.task('html:build', function(){
   return gulp.src(paths.html)
+    .pipe(plumber())
     .pipe(gulp.dest('./public/'))
-    .pipe(notify({message: 'HTML pages built'}));
-});
+    .pipe(connect.reload())
+    .pipe(notify({message: 'HTML pages built'}))
 
-gulp.task('html:watch', ['html:build'], function (done) {
-    browserSync.reload();
-    done();
 });
 
 gulp.task('css:build', function(){
   return gulp.src(paths.styles)
+    .pipe(plumber())
     .pipe(concat('styles.css'))
     .pipe(gulp.dest('./public/css'))
+    .pipe(connect.reload())
     .pipe(notify({message: 'CSS done'}));
-});
-
-gulp.task('css:watch', ['css:build'], function (done) {
-    browserSync.reload();
-    done();
 });
 
 gulp.task('build', ['html:build', 'js:build', 'css:build']);
 
 gulp.task('watch', function(){
-  gulp.watch(paths.html, ['html:watch']);
-  gulp.watch(paths.scripts, ['js:lint', 'js:watch']);
-  gulp.watch(paths.styles, ['css:watch']);
+  gulp.watch(paths.html, ['html:build']);
+  gulp.watch(paths.scripts, ['js:lint', 'js:build']);
+  gulp.watch(paths.styles, ['css:build']);
 });
 
-gulp.task('default', ['build', 'serve', 'watch']);
+gulp.task('default', ['build', 'connect', 'serve', 'watch']);
